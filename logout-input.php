1234a14ba90133b4043df8f9dc1850154dc6ca1c
ishizuka-kunimitu <62161459+ishizuka-kunimitu@users.noreply.github.com>
2020-05-09 11:24:54 +0900

    Update logout-input.php

--- logout-input.php
+++ logout-input.php
@@ -1,5 +1,8 @@
 <?php require '../header.php'; ?>
 <?php require 'menu.php'; ?>
-<p>ログアウトしますか？</p>
-<a href="logout-output.php">ログアウト</a>
+<form action="login-output.php" method="post">
+ログイン名<input type="text" name="login"><br>
+パスワード<input type="password" name="password"><br>
+<input type="submit" value="ログイン">
+</form>
 <?php require '../footer.php'; ?>
